4d3e093d28648cb56f1f2a2fa30a596deafe7f45
Mark Stout <72096913+mnstout@users.noreply.github.com>
2022-08-08 23:05:07 -0500

    Something that works

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from ctypes.wintypes import RGB
+from sqlite3 import Row
 from turtle import width
 from matplotlib.colors import rgb2hex
 from matplotlib.markers import MarkerStyle
@@ -75,27 +76,24 @@ def plot(df, name):
     ax[2,1].set_title(f"Adjusted Closing Value of {name}")
 
     plt.show()
+    
 
 # Return model and scalers
 def getModel(x,y):
     
-    if os.path.exists('model'):
+    if os.path.exists('RENAME TO MODEL TO JUST REUSE'):
         model = keras.models.load_model('model')
         
     else:
-        Xs = x[:,:,0:6]
-        Ys = y[:,:,3]
-        
-        Xs = np.asarray(Xs).astype('float32')
-        Ys = np.asarray(Ys).astype('float32')
-    
+        # Model Design
         model = Sequential()
-        model.add(LSTM(160, input_shape=(Xs.shape[1], Xs.shape[2])))
-        model.add(Dense(40))
-        model.add(Dense(20))
-        model.compile(loss='mae', optimizer='adam')
+        model.add(LSTM(120, return_sequences=True, input_shape= (x.shape[1], x.shape[2])))
+        model.add(LSTM(60, return_sequences=False))
+        model.add(Dense(30))
+        model.add(Dense(1))
+        model.compile(optimizer='adam', loss='mean_squared_error')
     
-        history = model.fit(Xs,Ys, epochs = 100, batch_size = 15)
+        history = model.fit(x,y, epochs = 50, batch_size = 20)
         plt.plot(history.history['loss'], label='train')
         plt.legend()
         plt.show()
@@ -104,6 +102,32 @@ def getModel(x,y):
         model.save('model')
     
     return model
+
+def ModelProcess(data, pred_days):
+    x = []
+    y = []
+    
+    print(data.shape[0])
+    
+    for i in range(pred_days, data.shape[0]):
+        x.append(data[i-pred_days:i, :])
+        y.append(data[i,:])
+        
+    x, y = np.array(x), np.array(y)
+    x = np.reshape(x, (x.shape[0], x.shape[1], 6))
+    
+    return x,y
+
+def testProcess(data, dataS, pred_days):
+    testShift = data.shape[0] - (pred_days)
+    x = []
+    y = data.to_numpy()[data.shape[0]-pred_days:, :]
+    for i in range(0, pred_days):
+        x.append(dataS[testShift+i-pred_days:i+testShift, :])
+    x = np.array(x)
+    x = np.reshape(x, (x.shape[0], x.shape[1], 6))
+    
+    return x,y
 
 # Import Data
 data = pd.read_csv (r'C:\Users\Nate\Desktop\CS539\dataset.csv', parse_dates=True)
@@ -115,72 +139,68 @@ with open("used_symbols.txt") as file:
     names = file.read().split('\n')
 
 ## Plot Random Data
-# plot(df, names[random.randint(0,len(names))])
-
-# Ensure all the names and symbols are linked properly by
-# deleting all elements in names that aren't in the dataset
-rElement = []
-for name in names:
-    if name not in df['Symbol'].values:
-        rElement.append(name)
-for r in rElement:
-    names.remove(r)
+#plot(df, names[random.randint(0,len(names))])
 
 ## Data prep for model
 # Open,High,Low,Close,Adj Close,Volume,Symbol,date
-x = []
-y = []
 
-# Scale
+###################- MODEL -########################
+
+# Choose a dataset to use for this run
+# ***** EDIT THE names[x] BELOW TO CHANGE COMPANY ***** # 
+Company = names[10]
+dC = df[df["Symbol"] == Company]
+dCD = dC.copy().drop(["Symbol","date"], axis =1)
+
+# Scale Dataset
 scaler = MinMaxScaler(feature_range=(0,1))
-df[["Open","High","Low","Close","Adj Close","Volume"]] = scaler.fit_transform(df[["Open","High","Low","Close","Adj Close","Volume"]])
+dCS = scaler.fit_transform(dCD)
 
+# Number of days to predict, train (All entries have 240-260 entries)
+pred_days = 30
+training_len = dCS.shape[0] - 30
+testing_len = 30
+dTr = dCS[0:training_len]
 
-for i in names:
-    tmp = df[df["Symbol"] == i]
-    if (tmp.shape[0] >= 60):
-        x.append(tmp[0:40])
-        y.append(tmp[40:60])
-
-x = np.reshape(np.array(x),(2110,40,8))
-y = np.reshape(np.array(y),(2110,20,8))
-
-# Split train/test set, not randomly selected
-xTrain = x[0:round((x.shape[0] * .75))]
-xTest = x[xTrain.shape[0]::]
-yTrain = y[0:round((y.shape[0] * .75))]
-yTest = y[yTrain.shape[0]::]
+# Prep Data for model
+x,y = ModelProcess(dTr, pred_days)
 
 # Get Trained Model
-model = getModel(xTrain,yTrain)
+model = getModel(x,y)
 
+# Prep Data for Testing
+x2,y2 = testProcess(dCD, dCS, pred_days)
 
+# Predict
+pred = model.predict(x2)
 
-
-xTest = xTest[:,:,0:6]
-xTest = np.asarray(xTest).astype('float32')
-pred = model.predict(xTest)
-
-pred = pred.reshape((1,pred.shape[0]*pred.shape[1]))
-zero = np.zeros((pred.shape[1],6))
-zero[:,3] = pred
+# Scale Properly
+zero = np.zeros(shape=(30,6))
+zero[:,3] = pred[:,0]
 pred = scaler.inverse_transform(zero)
-pred = pred[:,3].reshape((int(pred.shape[0]/20),20))
-
-for i in yTest:
-    i[:,0:6] = scaler.inverse_transform(i[:,0:6])
 
 
-yTest = yTest[:,:,3]
+# Prepare to Plot the data
+train = dC[:training_len]
+train.set_index(['date'])
+valid = dC[training_len:]
+valid['Predictions'] = pred[:,3]
 
-pd.DataFrame(pred).to_csv('pred.csv',index=False)
-pd.DataFrame(yTest).to_csv('real.csv',index=False)
+#fig,ax = plt.plot()
+#ax.set_xlabel('Date', fontsize=18)
+#ax.set_ylabel('Close Price USD ($)', fontsize=18)
+#ax.set_title('Model')
+#train.plot(x='date',y='Close', ax=ax)
+#valid.plot(x='date',y=['Close','Predictions'])
+#ax.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+#plt.show()
 
+# Visualize the data
 plt.figure(figsize=(16,6))
 plt.title('Model')
 plt.xlabel('Date', fontsize=18)
 plt.ylabel('Close Price USD ($)', fontsize=18)
-plt.plot(yTest[3])
-plt.plot(pred[3])
+plt.plot(train['date'],train['Close'])
+plt.plot(valid['date'],valid[['Close', 'Predictions']])
 plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
 plt.show()
